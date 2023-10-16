d576306cb4afc55b019c30f928d73203fde7bd0d
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-10-16 09:53:36 +0900

    1

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
 ﻿#include <stdio.h>
 
+void PrintfHelloWorld(void) {
+	printf("HelloWprld\n");
+}
+
+
+
 int main() {
 
-	
+	//関数ポインタ
+	//一言でいうと関数版のポインタ
+	printf("main=%p\n", main);
 
 
+	//宣言は変数と同じように関数の前に*をつけて()で括る
+	void (*pfunc) ();
 
-
-
+	//ここで中身を入れる
+	pfunc = PrintfHelloWorld;
+	printf("PrintfHelloWorldのアドレス=%p\n", PrintfHelloWorld);
+	printf("pfuncの内容=%p\n",*pfunc);
 
 	return 0;
 }
