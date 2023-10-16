6a71f38005b5ea9bc2ca688250d7cbaaa2738a5e
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-10-16 12:40:44 +0900

    動作確認OK!!!

--- Calculation/Calculation.h
+++ Calculation/Calculation.h
@@ -11,11 +11,4 @@ int Subtract(int a, int b);
 typedef void (*PFunc)(int*);
 
 
-//コールバック関数
-//他の関数に引数として渡す関数のこと
-//void DisplayResult(int *answer );
-
-//void SetTimeout(PFunc p, int second);
-
-
 int GetDiceNumber();
--- main.cpp
+++ main.cpp
@@ -49,51 +49,44 @@ int main() {
 #pragma endregion
 
 
-	//void DisplayResult(int *answer );
-	//std::function<void(int*)> DisplayResult = [](int *answer) {
-	//	//printf("%d秒待って実行されたよ\n", *s);
-	//
-	//	int rndNumber = GetDiceNumber();
-	//
-	//
-	//	printf("さいころの値は%dだよ\n", rndNumber);
-	//	
-	//	if (rndNumber % 2 == 0 && *answer%2==0) {
-	//		printf("正解!!\n");
-	//	}
-	//	else if (rndNumber % 2 == 1 && *answer%2==1) {
-	//		printf("正解!!\n");
-	//	}
-	//	else {
-	//		printf("不正解・・\n");
-	//	}
-	//};
+	
 	//
 	//PFunc wait;
 	//wait = DisplayResult(answer);
 
-	//void SetTimeout(PFunc p,int answer) {
-	//printf("さて答えはなんだろな\n\n");
-	////Sleepは待つ処理
-	////引数はミリ秒だよ
-	//Sleep(3 * 1000);
-	//
-	//
-	//
-	//p(&answer);
-	//}
 
-	//void SetTimeout(PFunc p, int second);
-
-	std::function<void(void)> SetTimeout = []() {
+	std::function<void()> SetTimeout = []() {
 		printf("さて答えはなんだろな\n\n");
 		//Sleepは待つ処理
 		//引数はミリ秒だよ
-		//Sleep(3 * 1000);
-		
+		Sleep(3 * 1000);
+		//p(&answer);
 	};
 
-	SetTimeout;
+
+	SetTimeout();
+
+	//void DisplayResult(int *answer );
+	std::function<void(int)> DisplayResult = [](int answer) {
+		//printf("%d秒待って実行されたよ\n", *s);
+	
+		int rndNumber = GetDiceNumber();
+	
+	
+		printf("さいころの値は%dだよ\n", rndNumber);
+		
+		if (rndNumber % 2 == 0 && answer%2==0) {
+			printf("正解!!\n");
+		}
+		else if (rndNumber % 2 == 1 && answer%2==1) {
+			printf("正解!!\n");
+		}
+		else {
+			printf("不正解・・\n");
+		}
+	};
+
+	DisplayResult(answer);
 
 	return 0;
 }
