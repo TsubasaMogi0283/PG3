a1a742d3f2e485ae94fcb9bc7b0e02074d58aace
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-10-30 17:52:06 +0900

    IShapeを作った

---
+++ IShape/Circle/Circle.h
@@ -0,0 +1,23 @@
+#pragma once
+#include 
+
+class Circle{ : public IShape
+public:
+
+	//ƒˆ‰¼‘zŠÖ”
+	virtual void Size()=0;
+
+
+	virtual void Draw()=0;
+
+
+
+
+
+private:
+
+
+
+
+};
+
---
+++ IShape/IShape.h
@@ -0,0 +1,21 @@
+#pragma once
+class IShape{
+public:
+
+	//ƒˆ‰¼‘zŠÖ”
+	virtual void Size()=0;
+
+
+	virtual void Draw()=0;
+
+
+
+
+
+private:
+
+
+
+
+};
+
