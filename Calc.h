1115710a12f0ccb38e43b085cda2112444028797
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-10-30 10:40:31 +0900

    02_02_確認課題

--- Calc.h
+++ Calc.h
@@ -1,5 +1,34 @@
 #pragma once
+
+template <typename Type>
+
 class Calc{
 
+public:
+	//()‚Ì’†g‚Íˆø”
+	Calc(Type n1, Type n2) :number1(n1), number2(n2) {
+
+	}
+
+
+	//¬‚³‚¢‚Ù‚¤‚ğ•Ô‚·
+	Type Min() {
+		if (number1 < number2) {
+			return number1;
+		}
+		else {
+			return number2;
+		}
+	}
+
+
+private:
+
+	Type number1;
+	Type number2;
+
+
+
 };
+
 
--- main.cpp
+++ main.cpp
@@ -1,15 +1,28 @@
 ﻿#include <stdio.h>
 #include "Template.h"
+#include "Calc.h"
 
 int main() {
 
-	
-	Template<int >b1(10, 20);
-	Template<double>b2(1.2, 3.4);
+#pragma region サンプル
+	//Template<int >b1(10, 20);
+	//Template<double>b2(1.2, 3.4);
+	//
+	//
+	//printf("面積: %d\n", b1.Size());
+	//printf("面積: %f\n", b2.Size());
 
+#pragma endregion 
 
-	printf("面積: %d\n", b1.Size());
-	printf("面積: %f\n", b2.Size());
+	//本編はここから
+
+	Calc<int >comper1(10, 20);
+	Calc<float>comper2(5.0f, 12.0f);
+	Calc<double>comper3(3.9, 0.5);
+
+	printf("int型で比較: %d\n", comper1.Min());
+	printf("float型で比較: %f\n", comper2.Min());
+	printf("doubleがたで比較: %f\n", comper3.Min());
 
 
 	return 0;
