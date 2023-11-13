ffb3e5ad0710822d3b790ae9392523ccb285cbc9
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-11-13 10:06:40 +0900

    訂正

--- main.cpp
+++ main.cpp
@@ -16,14 +16,24 @@ int main() {
 
 	//本編はここから
 
-	Calc<int >comper1(10, 20);
-	Calc<float>comper2(5.0f, 12.0f);
-	Calc<double>comper3(3.9, 0.5);
+	Calc<int >comperInt1(10, 20);
+	Calc<int >comperInt2(20, 15);
 
-	printf("int型で比較: %d\n", comper1.Min());
-	printf("float型で比較: %f\n", comper2.Min());
-	printf("doubleがたで比較: %f\n", comper3.Min());
+	Calc<float>comperFloat1(5.0f, 12.0f);
+	Calc<float>comperFloat2(20.0f, 57.0f);
+	
+	Calc<double>comperDouble1(3.9, 0.5);
+	Calc<double>comperDouble2(0.7, 1.2);
 
+
+	printf("int型で比較: %d\n", comperInt1.Min());
+	printf("int型で比較: %d\n", comperInt2.Min());
+
+	printf("float型で比較: %f\n", comperFloat1.Min());
+	printf("float型で比較: %f\n", comperFloat2.Min());
+
+	printf("doubleがたで比較: %f\n", comperDouble1.Min());
+	printf("doubleがたで比較: %f\n", comperDouble2.Min());
 
 	return 0;
 }
