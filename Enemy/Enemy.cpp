7385cb06a50d88a2987544e99bd09f1ef54c2752
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-12-18 13:46:44 +0900

    メンバ関数を作った

--- Enemy/Enemy.cpp
+++ Enemy/Enemy.cpp
@@ -1,59 +1,25 @@
-﻿//#include "Enemy.h"
-//#include <stdio.h>
-//
-//
-//Enemy::Enemy() {
-//
-//}
-//
-//void Enemy::Initialze() {
-//	phase_ = Phase::Approach;
-//}
-//
-////メンバ関数ポインタ
-//void (Enemy::* Enemy::spFunctable[])() = {
-//	//それぞれの関数
-//	&Enemy::Approach,
-//	&Enemy::Attack,
-//	&Enemy::Leave,
-//
-//
-//};
-//
-////近接
-//void Enemy::Approach() {
-//	printf("近接\n");
-//	//Sleep(3 * 1000);
-//	phase_ = Phase::Attack;
-//}
-//
-////射撃
-//void Enemy::Attack() {
-//	printf("射撃\n");
-//	//Sleep(3 * 1000);
-//	phase_ = Phase::Leave;
-//
-//}
-//
-////離脱
-//void Enemy::Leave() {
-//	printf("離脱\n");
-//}
-//
-////更新
-//void Enemy::Update() {
-//	//ループしないから気を付けて
-//	//for文じゃないとキツイっす
-//
-//	//現在フェーズの関数を実行
-//	//メンバ関数ポインタに入っている関数を呼び出す
-//	for (int i = 0; i < 3; i++) {
-//		(this->*spFunctable[static_cast<size_t>(phase_)])();
-//	}
-//	
-//}
-//
-//Enemy::~Enemy() {
-//
-//}
-//
+﻿#include "Enemy.h"
+
+Enemy::Enemy()
+{
+}
+
+void Enemy::Initialize()
+{
+}
+
+void Enemy::Aproach()
+{
+}
+
+void Enemy::Attack()
+{
+}
+
+void Enemy::Leave()
+{
+}
+
+void Enemy::Update()
+{
+}
--- Enemy/Enemy.h
+++ Enemy/Enemy.h
@@ -1,53 +1,3 @@
-//#pragma once
-//#include <Windows.h>
-//
-////typedef void (*PFunc)(void);
-//
-//class Enemy {
-//public:
-//	Enemy();
-//
-//	//初期化
-//	void Initialze();
-//
-//	
-//
-//	//更新
-//	void Update();
-//
-//	//近接
-//	void Approach();
-//
-//	//射撃
-//	void Attack();
-//
-//	//離脱
-//	void Leave();
-//
-//
-//	~Enemy();
-//
-//private:
-//	
-//
-//
-//private:
-//	
-//	//enum宣言
-//	enum class Phase {
-//		Approach,	//接近
-//		Attack,		//射撃
-//		Leave,		//離脱
-//	};
-//
-//	Phase phase_ = Phase();
-//
-//	//メンバ関数ポインタ
-//	static void (Enemy::* spFunctable[])();
-//
-//};
-
-
 //関数ポインタはその名前の通り関数を代入出来るポインタ
 //ポインタなので動的
 
@@ -66,8 +16,11 @@ public:
 	void Update();
 
 
+	~Enemy();
+
 private:
 
+	void (Enemy::* pFunc)();
 
 };
 
