3a4b0518f26c9458a96c6c1b828c631ad3c2a4d4
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-12-18 11:22:59 +0900

    1

--- Enemy/Enemy.h
+++ Enemy/Enemy.h
@@ -1,48 +1,75 @@
-#pragma once
-#include <Windows.h>
+//#pragma once
+//#include <Windows.h>
+//
+////typedef void (*PFunc)(void);
+//
+//class Enemy {
+//public:
+//	Enemy();
+//
+//	//初期化
+//	void Initialze();
+//
+//	
+//
+//	//更新
+//	void Update();
+//
+//	//近接
+//	void Approach();
+//
+//	//射撃
+//	void Attack();
+//
+//	//離脱
+//	void Leave();
+//
+//
+//	~Enemy();
+//
+//private:
+//	
+//
+//
+//private:
+//	
+//	//enum宣言
+//	enum class Phase {
+//		Approach,	//接近
+//		Attack,		//射撃
+//		Leave,		//離脱
+//	};
+//
+//	Phase phase_ = Phase();
+//
+//	//メンバ関数ポインタ
+//	static void (Enemy::* spFunctable[])();
+//
+//};
 
-//typedef void (*PFunc)(void);
+
+//関数ポインタはその名前の通り関数を代入出来るポインタ
+//ポインタなので動的
 
 class Enemy {
 public:
 	Enemy();
 
-	//初期化
-	void Initialze();
+	void Initialize();
 
-	
+	void Aproach();
 
-	//更新
-	void Update();
-
-	//近接
-	void Approach();
-
-	//射撃
 	void Attack();
 
-	//離脱
 	void Leave();
 
-
-	~Enemy();
-
-private:
-	
+	void Update();
 
 
 private:
-	
-	//enum宣言
-	enum class Phase {
-		Approach,	//接近
-		Attack,		//射撃
-		Leave,		//離脱
-	};
 
-	Phase phase_ = Phase();
-
-	//メンバ関数ポインタ
-	static void (Enemy::* spFunctable[])();
 
 };
+
+
+
