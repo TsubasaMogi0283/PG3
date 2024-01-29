8d3961e983e74c3760886d5ca8ac482e97867547
翼 茂木 <k022g0130@m.neec.ac.jp>
2024-01-29 11:27:12 +0900

    文字コード変えた

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <string>
 #include <chrono>
 #include <iostream>
@@ -35,18 +35,35 @@ int main() {
 #pragma endregion
 
 
-	std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+	
 	//10万文字の初期化の宣言はこうやってやるよ！
 	std::string a(100000, 'a');
 	
+#pragma region コピーの場合
+	std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
 	std::string a2 = a;
 	//現在の時間
 	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
 	auto result= std::chrono::duration_cast<std::chrono::microseconds>(end - now).count();
 
+	std::cout << "コピーの場合" << "\n";
 	std::cout<< result<<"\n";
 	
+
+#pragma region 移動の場合
+	std::chrono::steady_clock::time_point now2 = std::chrono::steady_clock::now();
+	std::string a3=std::move(a);
+	//現在の時間
+	std::chrono::steady_clock::time_point end2 = std::chrono::steady_clock::now();
+
+	auto result2 = std::chrono::duration_cast<std::chrono::microseconds>(end2 - now2).count();
+
+	std::cout << "移動の場合" << "\n";
+	std::cout << result2 << "\n";
+
+#pragma endregion
+
 
 	return 0;
 }
